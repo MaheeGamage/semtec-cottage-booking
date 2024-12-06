3ad7b8f5820e1a5fd56352539b79f62432a65da9
MaheeGamage <mahee.gamage@gmail.com>
2024-12-06 14:36:11 +0200

    SSWAP service url input added

--- mediator/src/main/webapp/myJS.js
+++ mediator/src/main/webapp/myJS.js
@@ -85,7 +85,42 @@ function doQuery_back(result) {
 	}
 }
 
+function initiModal() {
+	// Get the modal
+	var modal = document.getElementById("configModal");
 
+	// Get the button that opens the modal
+	var btn = document.getElementById("configBtn");
 
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
 
+	// When the user clicks the button, open the modal 
+	btn.onclick = function () {
+		modal.style.display = "block";
+	}
 
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function () {
+		modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function (event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+		}
+	}
+}
+
+function connectToService() {
+	var url = document.getElementById('sswapServiceUrl').value;
+	// Implement AJAX call to connect to the service
+	// On success:
+	document.getElementById('searchFormFieldSet').disabled = false;
+	document.getElementById('modalMessage').innerText = 'Connected successfully!';
+	document.getElementById('responseModal').style.display = 'block';
+}
+
+// Call the function to initialize the modal
+initiModal();
