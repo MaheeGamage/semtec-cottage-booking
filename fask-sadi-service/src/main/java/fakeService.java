ca375e8f334d507f74333f026c759dc02158ccae
MaheeGamage <mahee.gamage@gmail.com>
2024-12-07 16:57:15 +0200

    fake sadi service added

---
+++ fask-sadi-service/src/main/java/fakeService.java
@@ -0,0 +1,90 @@
+
+import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.OutputStream;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class fakeService
+ */
+@WebServlet("/fakeService")
+public class fakeService extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private static final String rdgFilePath = "/res/Group03/RDG.xml";
+	private static final String rrgFilePath = "/res/Group03/RRG.xml";
+
+	/**
+	 * Default constructor.
+	 */
+	public fakeService() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		response.setContentType("application/rdf+xml");
+		response.setCharacterEncoding("UTF-8");
+
+		// Path to the RDF file
+		String rdfFilePath = getServletContext().getRealPath(rdgFilePath);
+		File rdfFile = new File(rdfFilePath);
+
+		if (!rdfFile.exists()) {
+			response.sendError(HttpServletResponse.SC_NOT_FOUND, "RDF file not found.");
+			return;
+		}
+
+		// Write the RDF file to the response
+		try (FileInputStream fis = new FileInputStream(rdfFile); OutputStream out = response.getOutputStream()) {
+			byte[] buffer = new byte[1024];
+			int bytesRead;
+			while ((bytesRead = fis.read(buffer)) != -1) {
+				out.write(buffer, 0, bytesRead);
+			}
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		response.setContentType("application/rdf+xml");
+		response.setCharacterEncoding("UTF-8");
+
+		// Path to the RDF file
+		String rdfFilePath = getServletContext().getRealPath(rrgFilePath);
+		File rdfFile = new File(rdfFilePath);
+
+		if (!rdfFile.exists()) {
+			response.sendError(HttpServletResponse.SC_NOT_FOUND, "RDF file not found.");
+			return;
+		}
+
+		// Write the RDF file to the response
+		try (FileInputStream fis = new FileInputStream(rdfFile); OutputStream out = response.getOutputStream()) {
+			byte[] buffer = new byte[1024];
+			int bytesRead;
+			while ((bytesRead = fis.read(buffer)) != -1) {
+				out.write(buffer, 0, bytesRead);
+			}
+		}
+	}
+
+}
